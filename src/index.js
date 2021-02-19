6e13b712ff52ccc3ecde21f44bd9041948c473de
Frank <873634000@qq.com>
2021-02-19 17:41:09 +1000

    s

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const app = express();
 const {connectToDB} = require('./utils/db');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4000;
 
 app.use('/api',routes);
 connectToDB().then(()=>{
